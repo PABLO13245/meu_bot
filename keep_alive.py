6336847cf5ca513a8ffa1fbffaeb3cf71d325951
PABLO13245 <pablosilvapablo1234@gmail.com>
2025-10-22 22:05:02 -0300

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,24 +1,15 @@
 from flask import Flask
 from threading import Thread
-import logging
 
 app = Flask('')
 
-# Suppress Flask development server warning
-log = logging.getLogger('werkzeug')
-log.setLevel(logging.ERROR)
-
-
 @app.route('/')
 def home():
-    return "Bot is alive and running!"
-
+    return "Bot ativo!"
 
 def run():
-    app.run(host='0.0.0.0', port=5000)
-
+    app.run(host='0.0.0.0', port=8080)
 
 def keep_alive():
     t = Thread(target=run)
     t.start()
-    
