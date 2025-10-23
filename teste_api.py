d7caabee5c7239d561addd8a8174aca3dcb94d29
PABLO13245 <pablosilvapablo1234@gmail.com>
2025-10-23 14:24:16 -0300

    Update teste_api.py

--- teste_api.py
+++ teste_api.py
@@ -1,6 +1,6 @@
 import requests
 
-API_TOKEN = "EI7WanytFQuS2LmHvbBQ1fMDJjzcbXfsCmgWBQ62enNgPDaYCbwRjH5fj36W"
+API_TOKEN = "eNQYLjIAtZ5co7oMxlzyTPd4fb3s2lzRpDnQpNm9hoBL7sDoYr1HNHQKhPul"
 url = f"https://api.sportmonks.com/v3/football/fixtures/between/2025-10-23/2025-10-26?api_token={API_TOKEN}"
 
 print("Solicitação à SportMonks...")
