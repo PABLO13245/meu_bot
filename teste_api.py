0e9a552ff6eb4a656565e0d41897b72a1eef0b5a
PABLO13245 <pablosilvapablo1234@gmail.com>
2025-10-24 14:58:29 -0300

    Update teste_api.py

--- teste_api.py
+++ teste_api.py
@@ -1,8 +1,8 @@
 import os
-import requests
+import requests  # com "r" minúsculo
 
 # Tokens e IDs
-API_TOKEN = ("poBgEc23XAtTi2BEDIF7MmyY5BRAji5zkB1rAwTlXi1vA0B5NbuKJcLBK4sJ")  # seu token da SportMonks
+API_TOKEN = "poBgEc23XAtTi2BEDIF7MmyY5BRAji5zkB1rAwTlXi1vA0B5NbuKJcLBK4sJ"  # seu token da SportMonks
 TELEGRAM_TOKEN = "8444936746:AAE5JjO5vhrqb-HL7wWr-8kGpOjaCQybmgE"
 CHAT_ID = "5245918045"
 
@@ -24,10 +24,9 @@ if response.status_code == 200:
 
     texto = "\n\n".join(mensagens)
 
-    # Envia a mensagem para o Telegram
+    # Envia para o Telegram
     telegram_url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
     payload = {"chat_id": CHAT_ID, "text": texto}
-
     r = requests.post(telegram_url, data=payload)
     print("Mensagem enviada:", r.status_code)
 else:
