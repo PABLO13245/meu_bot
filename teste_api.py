4618ccaf1fc66f4d6ceb9f16f6e57ed1197bd4b6
PABLO13245 <pablosilvapablo1234@gmail.com>
2025-10-24 14:56:47 -0300

    Update teste_api.py

--- teste_api.py
+++ teste_api.py
@@ -2,7 +2,7 @@ import os
 import requests
 
 # Tokens e IDs
-API_TOKEN = os.getenv("poBgEc23XAtTi2BEDIF7MmyY5BRAji5zkB1rAwTlXi1vA0B5NbuKJcLBK4sJ")  # seu token da SportMonks
+API_TOKEN = ("poBgEc23XAtTi2BEDIF7MmyY5BRAji5zkB1rAwTlXi1vA0B5NbuKJcLBK4sJ")  # seu token da SportMonks
 TELEGRAM_TOKEN = "8444936746:AAE5JjO5vhrqb-HL7wWr-8kGpOjaCQybmgE"
 CHAT_ID = "5245918045"
 
