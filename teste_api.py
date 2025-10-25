85f1d02ccfd82479908223ee5c670f9757a8ae7f
PABLO13245 <pablosilvapablo1234@gmail.com>
2025-10-24 23:17:57 -0300

    Update teste_api.py

--- teste_api.py
+++ teste_api.py
@@ -1,13 +1,13 @@
 import os
-import requests  # com "r" minúsculo
+import requests
 
-# Tokens e IDs
-API_TOKEN = "poBgEc23XAtTi2BEDIF7MmyY5BRAji5zkB1rAwTlXi1vA0B5NbuKJcLBK4sJ"  # seu token da SportMonks
-TELEGRAM_TOKEN = "8444936746:AAE5JjO5vhrqb-HL7wWr-8kGpOjaCQybmgE"
-CHAT_ID = "5245918045"
+# Tokens e IDs (vindos das variáveis do Render)
+API_TOKEN = os.getenv("poBgEc23XAtTi2BEDIF7MmyY5BRAji5zkB1rAwTlXi1vA0B5NbuKJcLBK4sJ")
+TELEGRAM_TOKEN = os.getenv("8444936746:AAE5JjO5vhrqb-HL7wWr-8kGpOjaCQybmgE")
+CHAT_ID = os.getenv("5245918045")
 
-# URL da API da SportMonks (5 partidas)
-url = f"https://api.sportmonks.com/v3/football/fixtures?api_token={API_TOKEN}&include=league;season&page=1"
+# URL da API da SportMonks (pegando 5 partidas)
+url = f"https://api.sportmonks.com/v3/football/fixtures?api_token={API_TOKEN}&include=league,season&page=1"
 
 response = requests.get(url)
 print("Status:", response.status_code)
@@ -24,9 +24,10 @@ if response.status_code == 200:
 
     texto = "\n\n".join(mensagens)
 
-    # Envia para o Telegram
+    # Envia mensagem para Telegram
     telegram_url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
     payload = {"chat_id": CHAT_ID, "text": texto}
+
     r = requests.post(telegram_url, data=payload)
     print("Mensagem enviada:", r.status_code)
 else:
